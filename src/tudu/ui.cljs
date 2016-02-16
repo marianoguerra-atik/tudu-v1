4ad95f5b8166c06abe60f652b859537d83a3ba2a
Mariano Guerra <mariano@marianoguerra.org>
2016-02-16 17:27:33 +0100

    add initial state, read and mutate multimethod for counter
    the read default implementation does a lookup of the key in state if
    it finds it it returns the value otherwise retuns not-found.
    
    This is useful for local keys and we can use it to fetch :tudu/count in
    our case.
    
    To increment the counter we define mutate for tudu.counter/increment
    that takes a parameter called amount which is the amount to increment
    the counter.
    
    We also define our initial state as an atom with the initial counter
    value using defonce so it's not redefined when figwheel reloads changes.
    
    we pass the state and the parser (with the read and write multimethods
    as parameters) to create the reconciler.
    
    on button click we transact! passing the component as parameter (we can
    also pass the reconciler here but it's easier to pass the reference to
    the component) and then the query, in this case we call
    tudu.counter/increment and amount set to 1, after the call we pass the
    keys that we want to re read since they will change with this call.

--- src/tudu/ui.cljs
+++ src/tudu/ui.cljs
@@ -5,11 +5,39 @@
 
 (enable-console-print!)
 
+(defonce state (atom {:tudu/count 0}))
+
+(defn on-increment-click [component]
+  (om/transact! component
+                `[(tudu.counter/increment {:amount 1}) :tudu/count]))
+
 (defui UI
+  static om/IQuery
+  (query [this]
+         [:tudu/count])
   Object
   (render [this]
-          (dom/div nil "hello om.next")))
+          (let [{:keys [tudu/count]} (om/props this)]
+            (dom/div nil
+                     (dom/span nil "Count: " count)
+                     (dom/button #js {:onClick #(on-increment-click this)}
+                                 "Increment")))))
 
-(def reconciler (om/reconciler {}))
+(defmulti read om/dispatch)
+(defmulti mutate om/dispatch)
+
+(defmethod read :default [env key params]
+  (if-let [[_ value] (find @state key)]
+    {:value value}
+    :not-found))
+
+(defmethod mutate 'tudu.counter/increment
+  [{:keys [state] :as env} _ {:keys [amount]}]
+  {:action (fn [] (swap! state (fn [st]
+                                 (update st :tudu/count #(+ % amount)))))})
+
+(def reconciler (om/reconciler
+                  {:state state
+                   :parser (om/parser {:read read :mutate mutate})}))
 
 (om/add-root! reconciler UI (gdom/getElement "main-app-area"))
