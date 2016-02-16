830a617956e722607d5e367522fcb770b9603d18
Mariano Guerra <mariano@marianoguerra.org>
2016-02-16 17:05:13 +0100

    basic figwheel and cljsbuild setup
    to test run:
    
            lein figwheel
    
    and open http://localhost:3449/index.html or whatever figwheel says
    
    you should see an alert dialog with hello world, a string printed
    in the console (by prn) and one by console.log

--- project.clj
+++ project.clj
@@ -3,4 +3,29 @@
   :url "http://github.com/marianoguerra-atik/tudu"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [org.omcljs/om "1.0.0-alpha30"]]
+
+  :plugins [[lein-cljsbuild "1.1.2"]
+            [lein-figwheel "0.5.0-3"]]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                    "target"]
+
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src"]
+                        :figwheel true
+                        :compiler {:main       "tudu.ui"
+                                   :asset-path "js/compiled/out"
+                                   :output-to  "resources/public/js/compiled/tudu.js"
+                                   :output-dir "resources/public/js/compiled/out"
+                                   :source-map-timestamp true }}
+                       {:id "prod"
+                        :source-paths ["src"]
+                        :compiler {:main       "tudu.ui"
+                                   :asset-path "js/compiled/out"
+                                   :output-to  "resources/public/js/compiled/tudu.js"
+                                   :optimizations :advanced}}]}
+
+  :figwheel { :css-dirs ["resources/public/css"] })
---
+++ src/tudu/ui.cljs
@@ -0,0 +1,8 @@
+(ns tudu.ui)
+
+(enable-console-print!)
+
+(prn "hello world!")
+(.log js/console "hello console!")
+(.alert js/window "hello world!")
+
