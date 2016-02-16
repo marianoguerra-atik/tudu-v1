b6e7f0507578eb40cb5daddc0c3b467ea7ac4fa6
Mariano Guerra <mariano@marianoguerra.org>
2016-02-16 19:33:01 +0100

    allow creating a task
    NOTE: I had to pass reconciler to transact! instead of the component
    otherwise I would get an error similar to::
    
        Error: No queries exist for component path (tudu.ui/UI tudu.ui/NewTodoItemUI)
    
    To have a form to edit the new task we add a new key to the initial
    state called :tudu.item/editing which contains the state of the task we
    are editing, for now it's only the title, but later it could include
    something else.
    
    we add an input to enter the title, on change it sets the title on
    tudu.item/editing to the new value by calling the mutator tudu.item.editing/set-title
    
    on click of the button we call tudu.item/create passing the content of
    tudu.item/editing in the mutator we add a temp id (the current
    timestamp) and set the status to open.

--- src/tudu/ui.cljs
+++ src/tudu/ui.cljs
@@ -4,14 +4,18 @@
             [om.dom :as dom]))
 
 (enable-console-print!)
+(declare reconciler)
 
-(def initial-state {:tudu/items
+(def clean-item-editing {:title ""})
+
+(def initial-state {:tudu.item/editing clean-item-editing
+                    :tudu/items
                     [{:id 1 :status :close :title "Implement this Step"}
                      {:id 2 :status :open :title "Buy Food"}
                      {:id 3 :status :open :title "Implement next Step"}]})
 
-(defn close-item [component id]
-  (om/transact! component `[(tudu.item/close {:id ~id}) :tudu/items]))
+(defn close-item [c-or-r id]
+  (om/transact! c-or-r `[(tudu.item/close {:id ~id}) :tudu/items]))
 
 (defui TodoItemUI
   static om/IQuery
@@ -28,7 +32,7 @@
             (dom/div #js {:className "todo-list-item" :key (str "todo-item-" id)}
                      (dom/input #js {:type "checkbox" :disabled closed?
                                      :checked closed?
-                                     :onClick #(close-item this id)})
+                                     :onClick #(close-item reconciler id)})
 
                      (dom/span #js {:className class-name} title)))))
 
@@ -46,16 +50,53 @@
 
 (def todo-list-ui (om/factory TodoListUI))
 
+(defn on-change-cb [c-or-r callback]
+  (fn [e]
+    (let [target (.-target e)
+          value (.-value target)]
+      (callback c-or-r value))))
+
+(defn task-title-change [c-or-r text]
+  (om/transact! c-or-r `[(tudu.item.editing/set-title {:value ~text})
+                            :tudu.item/editing]))
+
+(defn create-task [c-or-r task]
+  (om/transact! c-or-r `[(tudu.item/create {:task ~task})
+                            :tudu.item/editing :tudu/items]))
+
+(defui NewTodoItemUI
+  static om/IQuery
+  (query [this]
+         [:tudu.item/editing])
+  Object
+  (render [this]
+          (let [{:keys [tudu.item/editing]} (om/props this)
+                {:keys [title]} editing
+                on-change (on-change-cb reconciler task-title-change)]
+            (dom/div #js {:className "new-todo-item-form"}
+                      (dom/div #js {:className "form-group"}
+                               (dom/label nil "Task")
+                               (dom/input #js {:value title
+                                               :onChange on-change}))
+                      ; NOTE: I have to pass the reconciler here and not the
+                      ; component otherwise I get:
+                      ; Error: No queries exist for component path (tudu.ui/UI tudu.ui/NewTodoItemUI)
+                      (dom/button #js {:onClick #(create-task reconciler editing)}
+                                  "Create")))))
+
+(def new-todo-item-ui (om/factory NewTodoItemUI))
+
 (defui UI
   static om/IQuery
   (query [this]
-         ; if I just return (om/get-query TodoListUI) I get
-         ; Error: Assert failed: Query violation
-         (vec (concat (om/get-query TodoListUI))))
+         (vec (concat (om/get-query TodoListUI)
+                      (om/get-query NewTodoItemUI))))
   Object
   (render [this]
           (let [props (om/props this)]
-            (todo-list-ui props))))
+            (dom/div nil
+                     (todo-list-ui props)
+                     (new-todo-item-ui props)))))
 
 (defmulti read om/dispatch)
 (defmulti mutate om/dispatch)
@@ -76,6 +117,22 @@
   {:action (fn []
              (swap! state
                     #(assoc-in % [:tudu.items/by-id id :status] :close)))})
+
+(defmethod mutate 'tudu.item.editing/set-title [{:keys [state]} _ {:keys [value]}]
+  {:action (fn []
+             (swap! state
+                    #(assoc-in % [:tudu.item/editing :title] value)))})
+
+(defmethod mutate 'tudu.item/create [{:keys [state]} _ {:keys [task]}]
+  {:action (fn []
+             (let [id (.getTime (js/Date.))
+                   full-task (assoc task :id id :status :open)]
+               (swap! state
+                      #(-> %
+                           (assoc :tudu.item/editing clean-item-editing)
+                           (assoc-in [:tudu.items/by-id id] full-task)
+                           (update :tudu/items
+                                   (fn [s] (conj s [:tudu.items/by-id id])))))))})
 
 (defonce reconciler (om/reconciler
                       {:state initial-state
